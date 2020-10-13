d6e6e753acd4143c9a00987faa0046e8580299a7
amarnath619 <53899621+amarnath619@users.noreply.github.com>
2020-10-13 13:37:01 +0530

    Add files via upload

---
+++ lab 4/assignment 1/commands.js
@@ -0,0 +1,11 @@
+db.books.save( {_id:1,Category:"M?chine Learning", Bookname:"Machine Learning for Hackers",Author: "Drew Conway",qty:25,price:400,rol:30,pages:350});
+
+db.books.save({_id:2,Category:"Business Intelligence", Bookname:"Fundamentals of Business Analytics",Author:"Seema Acharya",qty:55,price:500,rol:30,pages:250} );
+
+db.books.save({_id:3,Category:"Analytics", Bookname:"Competing on Analytics", Author:"Thomas Davenport",qty:8,price:150,rol:20,pages:150} );
+
+db.books.save({_id:4,Category:"Visualization", Bookname:"Visualizing Data", Author:"Ben Fry",qty:12,price:325,rol:6,pages:450});
+
+db.books.save({ _id:5,Category: "Web Mining", Bookname:" Learning R ", Author:" Richard",qty:15,price:850,rol:10,pages:120} );
+
+db.books.find({});
